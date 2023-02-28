ba1dac7bf03df53cc8a5f224c93df210afabf7ea
Adonay Tobar <tobaradonay@gmail.com>
2023-02-28 17:06:44 -0600

    Nueva version, pendiente de modificar.

--- main.js
+++ main.js
@@ -50,6 +50,22 @@ function agregarProducto() {
   let precioCompra = parseFloat(document.getElementById('precio-compra').value);
   let precioVenta = parseFloat(document.getElementById('precio-venta').value);
 
+   // Validar que los campos no estén vacíos
+   if (nombre === '' || isNaN(cantidad) || isNaN(precioCompra) || isNaN(precioVenta)) {
+    alert('Por favor, llene todos los campos antes de agregar el producto al inventario.');
+    return; // Salir de la función sin agregar el producto
+  }
+
+  // Validar que el producto no exista en el inventario
+  let productoExistente = inventario.find(function(producto) {
+    return producto.nombre.toLowerCase() === nombre.toLowerCase();
+  });
+
+  if (productoExistente) {
+    alert('Este producto ya existe en el inventario.');
+    return; // Salir de la función sin agregar el producto
+  }
+
   // Crear un objeto con los datos del producto
   let producto = {
     id: inventario.length + 1,
@@ -83,6 +99,8 @@ function mostrarInventario() {
   // Obtener el texto de búsqueda
   let textoBusqueda = document.getElementById('busqueda').value.toLowerCase();
 
+  let textoBusqueda1 = document.getElementById('busqueda1').value.toLowerCase();
+
   // Eliminar todos los elementos de las tablas excepto la fila de encabezado
   while (tablaCompleta.rows.length > 1) {
     tablaCompleta.deleteRow(1);
@@ -101,7 +119,13 @@ function mostrarInventario() {
     fila.insertCell().textContent = '$' + producto.precioCompra;
     fila.insertCell().textContent = '$' + producto.precioVenta;
     fila.insertCell().textContent = '$' + producto.ganancia;
+    
+    // Agregar evento de escucha de clic a la fila
+    fila.addEventListener('click', function() {
+      mostrarVentanaEditarProducto(producto);
+        });
   });
+
 
   // Agregar una fila por cada producto en el inventario que coincida con el texto de búsqueda
   inventario.forEach(function(producto) {
@@ -130,6 +154,123 @@ function mostrarInventario() {
 
 
 
+//Funcion para mostrar la ventana de editar productos
+function mostrarVentanaEditarProducto(producto){
+  
+
+  let ventana = document.createElement('div');
+  ventana.id = 'ventana-editar-producto';
+  document.body.appendChild(ventana);
+
+// Crear el formulario
+const formulario = document.createElement('form');
+
+// Crear los elementos del formulario
+const nombreLabel = document.createElement('label');
+nombreLabel.setAttribute('for', 'nombre');
+nombreLabel.textContent = 'Nombre:';
+
+const nombreInput = document.createElement('input');
+nombreInput.setAttribute('type', 'text');
+nombreInput.setAttribute('id', 'nombre');
+nombreInput.setAttribute('value', producto.nombre);
+
+const cantidadLabel = document.createElement('label');
+cantidadLabel.setAttribute('for', 'cantidad');
+cantidadLabel.textContent = 'Cantidad:';
+
+const cantidadInput = document.createElement('input');
+cantidadInput.setAttribute('type', 'number');
+cantidadInput.setAttribute('id', 'cantidad');
+cantidadInput.setAttribute('value', producto.cantidad);
+
+const precioCompraLabel = document.createElement('label');
+precioCompraLabel.setAttribute('for', 'precio-compraa');
+precioCompraLabel.textContent = 'Precio de compra:';
+
+const precioCompraInput = document.createElement('input');
+precioCompraInput.setAttribute('type', 'number');
+precioCompraInput.setAttribute('id', 'precio-compraa');
+precioCompraInput.setAttribute('step', "0.01");
+precioCompraInput.setAttribute('value', producto.precioCompra);
+
+const precioVentaLabel = document.createElement('label');
+precioVentaLabel.setAttribute('for', 'precio-ventaa');
+precioVentaLabel.textContent = 'Precio de venta:';
+
+const precioVentaInput = document.createElement('input');
+precioVentaInput.setAttribute('type', 'number');
+precioVentaInput.setAttribute('id', 'precio-ventaa');
+precioVentaInput.setAttribute('step', "0.01");
+precioVentaInput.setAttribute('value', producto.precioVenta);
+
+const botonSubmit = document.createElement('button');
+botonSubmit.setAttribute('type', 'submit');
+botonSubmit.textContent = 'Guardar cambios';
+
+const botonCancelar = document.createElement('button');
+botonCancelar.setAttribute('type', 'button');
+botonCancelar.textContent = 'Cancelar';
+
+// Agregar los elementos al formulario
+formulario.appendChild(nombreLabel);
+formulario.appendChild(nombreInput);
+formulario.appendChild(cantidadLabel);
+formulario.appendChild(cantidadInput);
+formulario.appendChild(precioCompraLabel);
+formulario.appendChild(precioCompraInput);
+formulario.appendChild(precioVentaLabel);
+formulario.appendChild(precioVentaInput);
+formulario.appendChild(botonSubmit);
+formulario.appendChild(botonCancelar);
+
+ventana.appendChild(formulario);
+
+  formulario.addEventListener('submit', function(event) {
+  event.preventDefault(); // Evitar que se recargue la página al hacer clic en el botón "Guardar cambios"
+
+      // Validar campos de entrada
+      const nombreValue = document.getElementById('nombre').value.trim();
+      const cantidadValue = document.getElementById('cantidad').value.trim();
+      const precioCompraValue = document.getElementById('precio-compraa').value.trim();
+      const precioVentaValue = document.getElementById('precio-ventaa').value.trim();
+  
+      if (!nombreValue || !cantidadValue || !precioCompraValue || !precioVentaValue) {
+        alert('Por favor, llene todos los campos antes de guardar los cambios.');
+        return;
+      }
+  
+      // Validar nombre del producto
+      const nombreExistente = inventario.find(p => p.nombre.toLowerCase() === nombreValue.toLowerCase());
+      if (nombreExistente && nombreExistente !== producto) {
+        alert('Ya existe un producto con el mismo nombre. Por favor, elija un nombre diferente.');
+        return;
+      }
+
+  producto.nombre = document.getElementById('nombre').value;
+  producto.cantidad = parseInt(document.getElementById('cantidad').value);
+  producto.precioCompra =parseFloat(document.getElementById('precio-compraa').value) ;
+  producto.precioVenta = parseFloat(document.getElementById('precio-ventaa').value);  
+  producto.ganancia = ((producto.precioVenta - producto.precioCompra) * producto.cantidad).toFixed(2); 
+  ventana.remove(); // Ocultar la ventana emergente
+  mostrarInventario(); // Actualizar la tabla de inventario
+
+  console.log(producto.precioCompra);
+  console.log(producto.nombre);
+  console.log(producto.ganancia);
+});
+
+// Agregar evento click al botón de cancelar
+botonCancelar.addEventListener('click', function() {
+  ventana.remove(); // Ocultar la ventana emergente
+});
+
+
+
+}
+
+
+
 
 
 
@@ -139,14 +280,9 @@ function mostrarInventario() {
 
 let carrito = []; // Array para almacenar los productos seleccionados
 let ventas = [];
+let totalCompra = 0;
 
 function agregarAlCarrito(producto) {
-
-  // Validar que hay suficiente cantidad en el inventario
-  if (producto.cantidadEnInventario < 1) {
-    alert('No hay suficiente cantidad en el inventario para este producto.');
-    return;
-  }
 
   // Agregar el producto al array del carrito
   let productoCarrito = {
@@ -155,6 +291,8 @@ function agregarAlCarrito(producto) {
     precioVenta: producto.precioVenta,
     cantidad: 1 // Establecer la cantidad inicial a 1
   };
+  carrito.push(productoCarrito);
+
 
   // Mostrar los productos del carrito en una tabla
   let tablaCarrito = document.getElementById('tabla-carrito');
@@ -179,6 +317,37 @@ function agregarAlCarrito(producto) {
     // Actualizar el precio total al cambiar la cantidad
     let precioTotal = this.value * producto.precioVenta;
     fila.cells[3].textContent = '$' + precioTotal.toFixed(2);
+    
+    // Actualizar el total de la compra
+totalCompra = 0; // Inicializar el total en 0
+carrito.forEach(function(producto) {
+  totalCompra += producto.precioVenta * producto.cantidad;
+});
+
+// Insertar fila con el total de la compra si no existe
+let filaTotal = tablaCarrito.querySelector(".total"); // Buscar la fila del total
+if (!filaTotal) { // Si la fila del total no existe, crearla
+  filaTotal = tablaCarrito.insertRow(); // Añadir una nueva fila al final de la tabla
+  filaTotal.insertCell().colSpan = 2; // Añadir una celda que ocupe 2 columnas
+  filaTotal.insertCell().textContent = "Total";
+  filaTotal.insertCell().textContent = "$" + totalCompra.toFixed(2); // Añadir una celda con el total de la compra
+  filaTotal.classList.add("total"); // Agregar una clase para identificar la fila del total
+}
+else { // Si la fila del total existe, actualizar su valor
+  filaTotal.cells[2].textContent = "$" + totalCompra.toFixed(2); // Actualizar el valor de la celda del total de la compra
+}
+
+// Mover la fila del total al final de la tabla
+tablaCarrito.appendChild(filaTotal);
+
+
+
+  
+    
+
+
+    console.log(totalCompra);
+
 
   });
   fila.insertCell().appendChild(cantidadEditable);
@@ -191,6 +360,8 @@ function agregarAlCarrito(producto) {
   
   // Insertar celda de precio total
   fila.insertCell().textContent = '$' + producto.precioVenta.toFixed(2);
+
+
 
   
   // Agregar botón al final de la tabla para descontar cantidades del inventario
@@ -226,7 +397,6 @@ function agregarAlCarrito(producto) {
     mostrarVentas();
   });
 
-  carrito.push(productoCarrito);
 }
 
 
@@ -288,6 +458,11 @@ mostrarInventario();
 
 // Agregar un evento de escucha al campo de entrada para actualizar la tabla en función del texto de búsqueda
 let campoBusqueda = document.getElementById('busqueda');
+campoBusqueda.addEventListener('input', function() {
+  mostrarInventario();
+});
+
+let campoBusqueda1 = document.getElementById('busqueda1');
 campoBusqueda.addEventListener('input', function() {
   mostrarInventario();
 });
